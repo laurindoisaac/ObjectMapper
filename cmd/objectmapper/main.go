8f9e297fed6c33f5f1b83b0eb45f125c520447aa
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-09 23:48:12 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/objectmapper/main.go
@@ -0,0 +1,23 @@
+// cmd/objectmapper/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "objectmapper/internal/objectmapper"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := objectmapper.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
